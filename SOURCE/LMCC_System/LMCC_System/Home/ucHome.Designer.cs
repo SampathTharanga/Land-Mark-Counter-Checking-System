33c6f3cf3bab7e6791dbb391df1b81c9749a2c56
Sampath Tharanga <sampaththaranga13@gmail.com>
2020-04-14 15:34:35 +0530

    changing home window

--- SOURCE/LMCC_System/LMCC_System/Home/ucHome.Designer.cs
+++ SOURCE/LMCC_System/LMCC_System/Home/ucHome.Designer.cs
@@ -29,43 +29,64 @@
         private void InitializeComponent()
         {
             this.panel4 = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pieChart2 = new LiveCharts.WinForms.PieChart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.pieChart1 = new LiveCharts.WinForms.PieChart();
             this.btnHome = new System.Windows.Forms.Button();
             this.groupBox6 = new System.Windows.Forms.GroupBox();
             this.label17 = new System.Windows.Forms.Label();
             this.label16 = new System.Windows.Forms.Label();
-            this.pieChart2 = new LiveCharts.WinForms.PieChart();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.panel4.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.groupBox6.SuspendLayout();
-            this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox6.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel4
             // 
-            this.panel4.BackColor = System.Drawing.Color.White;
-            this.panel4.Controls.Add(this.panel1);
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(209)))), ((int)(((byte)(219)))));
+            this.panel4.Controls.Add(this.groupBox6);
+            this.panel4.Controls.Add(this.btnHome);
+            this.panel4.Controls.Add(this.groupBox1);
+            this.panel4.Controls.Add(this.groupBox2);
             this.panel4.Location = new System.Drawing.Point(10, 10);
             this.panel4.Margin = new System.Windows.Forms.Padding(0);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(747, 621);
             this.panel4.TabIndex = 2;
             // 
-            // panel1
+            // groupBox2
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(132)))), ((int)(((byte)(209)))), ((int)(((byte)(219)))));
-            this.panel1.Controls.Add(this.groupBox2);
-            this.panel1.Controls.Add(this.groupBox1);
-            this.panel1.Controls.Add(this.btnHome);
-            this.panel1.Controls.Add(this.groupBox6);
-            this.panel1.Location = new System.Drawing.Point(14, 15);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(719, 591);
-            this.panel1.TabIndex = 0;
+            this.groupBox2.Controls.Add(this.pieChart2);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(376, 265);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(339, 340);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Settle and Non Settle List";
+            // 
+            // pieChart2
+            // 
+            this.pieChart2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pieChart2.ForeColor = System.Drawing.Color.White;
+            this.pieChart2.Location = new System.Drawing.Point(3, 16);
+            this.pieChart2.Name = "pieChart2";
+            this.pieChart2.Size = new System.Drawing.Size(333, 321);
+            this.pieChart2.TabIndex = 6;
+            this.pieChart2.Text = "pieChart2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pieChart1);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(39, 265);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(324, 340);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Land Mark Stock";
             // 
             // pieChart1
             // 
@@ -84,7 +105,7 @@
             this.btnHome.ForeColor = System.Drawing.Color.White;
             this.btnHome.Image = global::LMCC_System.Properties.Resources.Backup;
             this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnHome.Location = new System.Drawing.Point(437, 44);
+            this.btnHome.Location = new System.Drawing.Point(30, 45);
             this.btnHome.Margin = new System.Windows.Forms.Padding(6, 0, 0, 0);
             this.btnHome.Name = "btnHome";
             this.btnHome.Padding = new System.Windows.Forms.Padding(6, 0, 0, 0);
@@ -97,7 +118,7 @@
             // 
             this.groupBox6.Controls.Add(this.label17);
             this.groupBox6.Controls.Add(this.label16);
-            this.groupBox6.Location = new System.Drawing.Point(32, 27);
+            this.groupBox6.Location = new System.Drawing.Point(349, 20);
             this.groupBox6.Name = "groupBox6";
             this.groupBox6.Size = new System.Drawing.Size(351, 141);
             this.groupBox6.TabIndex = 2;
@@ -108,7 +129,7 @@
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label17.ForeColor = System.Drawing.Color.White;
-            this.label17.Location = new System.Drawing.Point(18, 18);
+            this.label17.Location = new System.Drawing.Point(6, 25);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(323, 54);
             this.label17.TabIndex = 1;
@@ -119,43 +140,11 @@
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label16.ForeColor = System.Drawing.Color.White;
-            this.label16.Location = new System.Drawing.Point(67, 93);
+            this.label16.Location = new System.Drawing.Point(63, 93);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(225, 25);
             this.label16.TabIndex = 0;
             this.label16.Text = "Thursday, April 02, 2020";
-            // 
-            // pieChart2
-            // 
-            this.pieChart2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pieChart2.ForeColor = System.Drawing.Color.White;
-            this.pieChart2.Location = new System.Drawing.Point(3, 16);
-            this.pieChart2.Name = "pieChart2";
-            this.pieChart2.Size = new System.Drawing.Size(333, 321);
-            this.pieChart2.TabIndex = 6;
-            this.pieChart2.Text = "pieChart2";
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.pieChart1);
-            this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Location = new System.Drawing.Point(22, 233);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(324, 340);
-            this.groupBox1.TabIndex = 7;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Land Mark Stock";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.pieChart2);
-            this.groupBox2.ForeColor = System.Drawing.Color.White;
-            this.groupBox2.Location = new System.Drawing.Point(370, 233);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(339, 340);
-            this.groupBox2.TabIndex = 8;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Settle and Non Settle List";
             // 
             // ucHome
             // 
@@ -166,18 +155,16 @@
             this.Name = "ucHome";
             this.Size = new System.Drawing.Size(767, 641);
             this.panel4.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
             this.groupBox6.ResumeLayout(false);
             this.groupBox6.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.GroupBox groupBox6;
         private System.Windows.Forms.Label label17;
         private System.Windows.Forms.Label label16;
